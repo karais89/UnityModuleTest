02da84df9e5e5b04fd3d0d7033b1e8562ca392cf
kaya <jihun3@treenod.com>
2019-08-19 12:10:05 +0900

    test

---
+++ Packages/UnityModuleTest/Runtime/RuntimeExampleTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeExampleTest
+{
+    public static void Test()
+    {
+        Debug.Log("Test");
+    }
+}
